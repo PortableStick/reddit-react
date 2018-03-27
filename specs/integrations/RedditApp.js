affdeb535fc54106908e639d39914e5edcfd4f0b
PortableStick <crashingwaves@fea.st>
2018-03-27 16:32:19 -0600

    Test for title

--- specs/integrations/RedditApp.js
+++ specs/integrations/RedditApp.js
@@ -10,5 +10,10 @@ describe('RedditApp', () => {
   it('should render to the page', () => {
     expect(browser.isExisting('.App')).to.equal(true);
   });
+
+  it('should have the correct title', () => {
+    const actualTitle = browser.getTitle();
+    expect(actualTitle).to.equal('Reddit Viewer');
+  });
 });
 
