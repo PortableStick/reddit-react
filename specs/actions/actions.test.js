369b200076c0de506a711f25324a52fcaf684383
PortableStick <crashingwaves@fea.st>
2018-03-28 19:36:27 -0600

    Action tests

--- specs/actions/actions.test.js
+++ specs/actions/actions.test.js
@@ -14,4 +14,27 @@ describe('Action creators', () => {
       expect(actions.fetchPostsFromSubreddit(subreddit)).toEqual(expectedAction);
     });
   });
+
+  describe('receivePosts', () => {
+    it('should create an action to update posts', () => {
+      const expectedAction = {
+        type: types.RECEIVE_POSTS,
+        payload: [],
+      };
+
+      expect(actions.receivePosts([])).toEqual(expectedAction);
+    });
+  });
+
+  describe('handleError', () => {
+    it('should create an action to handle an error', () => {
+      const err = new Error('not an error lol');
+      const expectedAction = {
+        type: types.HANDLE_ERROR,
+        payload: err,
+      };
+
+      expect(actions.handleError(err)).toEqual(expectedAction);
+    });
+  });
 });
